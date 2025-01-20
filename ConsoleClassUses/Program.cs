003bdd2d335c9045f611ad72bdeb92610520a1bd
Mohit <94623283+ErElectrical@users.noreply.github.com>
2025-01-20 01:07:52 -0800

    Add files via upload
    In this tutorial we will learn basic structure of a C# program  and uses of console application

---
+++ ConsoleClassUses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Title = "Console classes Uses";
+            Console.WriteLine("Devloper name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Age:");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Your name is: "+name);
+            Console.WriteLine("Your name is: " + age);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
